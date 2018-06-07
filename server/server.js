ecd26f5f02cd31a15a0944fa071e678798f69ce5
antonioGodinez <rudx_inc@hotmail.com>
2018-06-07 16:29:15 -0700

    Added POST /users

--- server/server.js
+++ server/server.js
@@ -5,12 +5,14 @@ const bodyParser = require('body-parser');
 const { mongoose } = require('./db/mongoose');
 
 const TodoRoutes = require('./routes/todo');
+const UserRoutes = require('./routes/user');
 
 var app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use('/', TodoRoutes.routes);
+app.use('/', UserRoutes.routes);
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
